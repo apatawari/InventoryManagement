96586d94140dfcee9fdf51501f2485a8d31ae3b7
Shardul <shardul.shetye16gmail.com>
2020-10-18 11:44:54 +0530

    first commit

---
+++ inventoryManagement/inventoryapp/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+
+# Create your models here.
+
+class Item(models.Model):
+    sr_no = models.IntegerField()
+    party_name = models.CharField(max_length=50)
+    bill_no = models.IntegerField()
+    #bill_date = models.DateField()
+    bill_amount = models.FloatField(max_length=15)
+    lot_no = models.IntegerField()
+    quality = models.CharField(max_length=100)
+    than = models.IntegerField()
+    mtrs = models.FloatField(max_length=15)
+    bale = models.IntegerField()
+    rate = models.FloatField(max_length=15)
+    lr_no = models.IntegerField()
+    order_no = models.IntegerField()
+    state_choices=(('state1','In transit'), ('state2','Order recieved'), ('state3','In godown'), ('state4','done'))
+    state = models.CharField(max_length=10,choices=state_choices,default='-')
+    
+    def __str__(self):
+        return self.sr_no +" " +self.party_name
