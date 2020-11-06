dd47f0a652dc2bf47a5c75e8d0efbf1670fb0efc
Devdoot-Maji <devdootmaji@gmail.com>
2020-11-06 17:51:43 +0530

    Django-Debugger Removed

--- inventoryManagement/inventoryapp/urls.py
+++ inventoryManagement/inventoryapp/urls.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import path
 from . import views
-import debug_toolbar
 
 from django.conf.urls import (
     handler400,
@@ -74,6 +73,4 @@ urlpatterns = [
     path('export/excelpage', views.export_page_xls, name='excel_page'),
     path('export/excelall', views.export_all_xls, name='excel_all'),
     path('export/excelfilter', views.export_filter_all_xls, name='excel_filter_all'),
-
-    path('__debug__/', include(debug_toolbar.urls)),
 ]
